7346df77420e89c620ca51a1009f9f2f74e7ea9e
pigram86 <todd@toddpigram.com>
2013-12-02 12:40:17 -0500

    attr added

---
+++ metadata.rb
@@ -0,0 +1,9 @@
+name             'firefox'
+maintainer       'Todd Pigram'
+maintainer_email 'todd@toddpigram.com'
+license          'Apache 2.0'
+description      'Installs/Configures firefox'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '1.1.0'
+depends          "windows"
+
